0adf3dab0e98d300524165847e3b5ee2266ba9fd
Oyon87 <oyon87@yahoo.com>
2021-12-27 23:51:24 +0700

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,81 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<div class="container" style="margin-top: 20px;">
+		<div class="card">
+			<div class="card-body">
+				<h5 class="card-title text-center">SIMPLE TODO APP</h5>
+				<div class="row">
+					<div class="col-10">
+						<input type="text" class="form-control" v-model="todo" @keyup.enter="add">
+					</div>
+					<div class="col-2">
+						<button class="btn btn-success" @click="add">ADD</button>
+					</div>
+				</div>
+				<List class="mb-3" :lists="lists" @deleteTodo="deleteTodo" @doneTodo="doneTodo" />
+				<small>Total TODO : {{ totalTODO }}</small>
+			</div>
+		</div>	
+	</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import { ref, toRefs, reactive, computed, onMounted } from "vue";
+import List from "./components/List.vue";
+
+export default {
+	components: { List },
+	setup() {
+		const todo = ref('');
+		const todos = reactive({
+			lists: [],
+		});
+
+		onMounted(() => {
+			const items = localStorage.getItem('todos');
+			todos.lists = items ? JSON.parse(items) : [];
+		});
+
+		const totalTODO = computed(() => {
+			return todos.lists.length;
+		});
+
+		const add = () => {
+			if(todo.value != ''){
+				todos.lists.unshift({
+				activity: todo.value,
+				isDone: false
+			});
+				todo.value = "";
+				saveToLocalStorage();
+			}	
+		};
+
+		const deleteTodo = todoIndex => {
+			todos.lists = todos.lists.filter((list, index) => {
+				if(index != todoIndex){
+					return list;
+				}
+			});
+			saveToLocalStorage();
+		};
+
+		const doneTodo = todoIndex => {
+			todos.lists = todos.lists.filter((list, index) => {
+				if(index == todoIndex){
+					list.isDone ? list.isDone = false : list.isDone = true;
+				}
+				return list;
+			});
+			saveToLocalStorage();
+		};
+
+		const saveToLocalStorage = () => {
+			localStorage.setItem('todos', JSON.stringify(todos.lists));
+		};
+
+		return{
+			todo, ...toRefs(todos), add, deleteTodo, totalTODO, doneTodo, saveToLocalStorage,
+		};
+	},
 }
-</style>
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,46 @@
+<template>
+	<ul class="list-group mt-4">
+		<li class="list-group-item" v-for="(list, index) in lists" :key="index">
+			<div class="row align-items-center">
+				<div class="col">
+					<span v-if="list.isDone"><del>{{ list.activity }}</del></span>
+					<span v-else>{{ list.activity }}</span>
+				</div>
+				<div class="col-auto">
+					<div class="row">
+						<div class="col p-1">
+							<button class="btn btn-info" @click="doneTODO(index)">Done</button>
+						</div>
+						<div class="col p-1">
+							<button class="btn btn-danger" @click="deleteTODO(index)">X</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: {
+		lists: {
+			type: Array,
+			default: []
+		}
+	},
+	setup(props, { emit }) {
+		const deleteTODO = index => {
+			emit('deleteTodo', index);
+		};
+
+		const doneTODO = index => {
+			emit('doneTodo', index);
+		};
+
+		return {
+			deleteTODO, doneTODO,
+		}
+	},
+}
+</script>
